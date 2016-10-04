a73e12d326ae09325b695abd779a90c8867e0a49
black <black@DESKTOP-IAP7KVH>
2016-10-04 08:40:59 +0200

    fix, *Asset.cs

--- 11_Files/IStockRepository.cs
+++ 11_Files/IStockRepository.cs
@@ -4,10 +4,10 @@ namespace _11_Files
 {
     interface IStockRepository
     {
-        long        NextId();
-        void        SaveStock(Stock stock);
-        Stock       LoadStock(long id);
-        void        Clear();
-        ICollection FindAllStocks();
+        long               NextId();
+        void               SaveStock(Stock stock);
+        Stock              LoadStock(long id);
+        void               Clear();
+        ICollection        FindAllStocks();
     }
 }
